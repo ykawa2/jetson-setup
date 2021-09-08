52aedffb62751289cad71dc5bdf91ad08d5859e6
ykawa2 <88624008+ykawa2@users.noreply.github.com>
2021-09-08 15:53:01 +0900

    first commit
    .

---
+++ setup_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Add
+sudo apt update
+sudo apt upgrade
+
+#Setup pip and docker compose
+sudo apt install curl python3-testresources
+curl -kL https://bootstrap.pypa.io/get-pip.py | python3
+python3 -m pip install --user docker-compose
+
+echo "" >> ~/.bashrc
+echo #added for docker compose >> ~/.bashrc
+echo PATH=$PATH:$HOME/.local/bin >> ~/.bashrc
+source ~/.bashrc
+
+sudo gpasswd -a $USER docker
+
+docker-compose --version
+
+
