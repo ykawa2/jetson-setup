a684a2c311fcca67993071bf7be50e7f77a70df9
ykawa2 <88624008+ykawa2@users.noreply.github.com>
2021-09-08 16:01:56 +0900

    first commit
    .

---
+++ setup_github.sh
@@ -0,0 +1,25 @@
+
+
+#Create SSH key (~/.ssh/id_ed25519 and .pub)
+ssh-keygen -t ed25519 –C "<email>"
+
+#launch ssh-agent
+eval "$(ssh-agent -s)" 
+
+#Edit ~/.ssh/config
+Echo "Host github.com" >> ~/.ssh/config
+Echo "  HostName github.com" >> ~/.ssh/config
+Echo "  IdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
+Echo "  AddKeysToAgent yes" >> ~/.ssh/config
+Echo "  User git" >> ~/.ssh/config
+
+#Register ssh key to ssh-agent
+ssh-add -k ~/.ssh/id_ed25519
+
+git config --global user.name "<username>"
+git config --global user.email "<email>"
+
+#Show results and proceed to registration on github.
+git config --global --list
+cat ~/.ssh/id_ed25519.pub
+echo "Please regsiter new SSH key on account settings of github"
