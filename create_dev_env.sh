ca52068fcccc468fbc28a7036288b5e03834268a
ykawa2 <88624008+ykawa2@users.noreply.github.com>
2021-09-11 15:37:54 +0900

    Update create_dev_env.sh

--- create_dev_env.sh
+++ create_dev_env.sh
@@ -3,3 +3,8 @@
 #enable external sdcard (exfat)
 # ?sudo add-apt-repository universe
 sudo apt install exfat-fuse exfat-utils
+
+#set python3 as default
+sudo unlink python
+sudo ln -s python3 python
+
