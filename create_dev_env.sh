3b653256de176ccdc242a4b0f7ba825976e1d654
ykawa2 <88624008+ykawa2@users.noreply.github.com>
2021-09-11 16:00:47 +0900

    Update create_dev_env.sh

--- create_dev_env.sh
+++ create_dev_env.sh
@@ -5,6 +5,11 @@
 sudo apt install exfat-fuse exfat-utils
 
 #set python3 as default
+cd /usr/bin
 sudo unlink python
 sudo ln -s python3 python
 
+#set utilities for jetson
+git clone https://github.com/jetsonhacks/jetsonUtilities
+cd jetsonUtilities
+python jetsonInfo.py
