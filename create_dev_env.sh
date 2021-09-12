99bb205cb45e709f609f07e5502736b0ceb97e41
ykawa2 <88624008+ykawa2@users.noreply.github.com>
2021-09-12 20:36:28 +0900

    Update create_dev_env.sh

--- create_dev_env.sh
+++ create_dev_env.sh
@@ -18,3 +18,20 @@ cd ~
 git clone https://github.com/jetsonhacks/jetsonUtilities
 cd jetsonUtilities
 python jetsonInfo.py
+
+#Jetson stats (launch with jtop)
+sudo apt install python-pip
+sudo pip install jetson-stats
+echo "Reboot to enable jetson-stats"
+
+#Jetson thermal monitor 
+#Usage: cd ~/jetson-thermal-monitor/ -> python jetson_temp_monitor.py
+sudo apt install python3-pip libfreetype6-dev python3-numpy python3-matplotlib
+git clone https://github.com/tsutof/jetson-thermal-monitor
+
+#Setting for remote desktop environment
+sudo apt install xrdp
+echo lxsession > ~/.xsession
+echo "Set IP address manually and after chaging the IP adress, you can connect this device from remote desktop."
+
+
