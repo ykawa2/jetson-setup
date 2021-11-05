d941437b45abc58d171bbc8dfd41d10a498db61a
ykawa2 <88624008+ykawa2@users.noreply.github.com>
2021-11-05 22:34:15 +0900

    Update create_dev_env.sh

--- create_dev_env.sh
+++ create_dev_env.sh
@@ -5,21 +5,25 @@ sudo apt update
 sudo apt upgrade -y
 
 #SDcard settings----------------------------------------------------
+echo ""
 echo ">>> Installing utils for external sdcard (format: exfat)"
 sudo apt install -y exfat-fuse exfat-utils
 
 #python settings----------------------------------------------------
+echo ""
 echo ">>> Setting python3 as default"
 cd /usr/bin
 sudo unlink python
 sudo ln -s python3 python
 
 #UnixBench----------------------------------------------------------
+echo ""
 echo ">>> Downloading UnixBench"
 cd ~
 git clone https://github.com/kdlucas/byte-unixbench
 
 #jetsonUtilities----------------------------------------------------
+echo ""
 echo ">>> Installing jetsonUtilities"
 cd ~
 git clone https://github.com/jetsonhacks/jetsonUtilities
@@ -27,24 +31,28 @@ cd jetsonUtilities
 python jetsonInfo.py
 
 #Jetson stats-------------------------------------------------------
+echo ""
 echo ">>> Installing Jetson stats (launch with jtop)"
 sudo apt install python-pip
 sudo pip install jetson-stats
 echo ">>> Reboot to enable jetson-stats"
 
 #jetson thermal monitor---------------------------------------------
+echo ""
 echo ">>> Installing jetson thermal monitor "
 #Usage: cd ~/jetson-thermal-monitor/ -> python jetson_temp_monitor.py
 sudo apt install -y python3-pip libfreetype6-dev python3-numpy python3-matplotlib
 git clone https://github.com/tsutof/jetson-thermal-monitor
 
 #Desktop env--------------------------------------------------------
+echo ""
 echo ">>> Setting for remote desktop environment"
 sudo apt install -y xrdp
 echo lxsession > ~/.xsession
 echo ">>> Set IP address manually and after chaging the IP adress, you can connect this device from remote desktop."
 
 #Docker-Compose settings--------------------------------------------
+echo ""
 echo ">>> Setup pip and docker compose"
 sudo apt install curl python3-testresources
 curl -kL https://bootstrap.pypa.io/get-pip.py | python3
